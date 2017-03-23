cf36d228e185273f173195d55d89992138935655
xuqingsen <gulangduxiao@gmail.com>
2017-03-23 23:43:41 +0800

    新增update参数
    将涉及到修改的脚本文件及静态资源，单独拷贝到指定的目录下，方便后期统一更新到服务器

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@
 │	│		├── *.ftl 
 *
 *文件路径配置
+*update:替换后的文件输出目录，方便后期统一更新到服务器,
 *src: webapp目录，绝对路径，需要自行配置
 *page: 页面根目录，相对路径，需要自行配置
 *resource：静态资源，相对路径，需要自行配置
@@ -28,6 +29,7 @@
 *app：静态资源临时目录，无需修改
 */
 var config = {
+    update:'D:\\codes\\testWb\\update\\',
     src: 'D:\\codes\\testWb\\',
     page: '',
 	resource: '**/*.{js,css,png,jpg,jpeg,gif,bmp,ico,eot,svg,ttf,woff}',
@@ -64,7 +66,7 @@ function() {
 //替换引用
 gulp.task('revCollector',
 function() {
-    return gulp.src([config.rev + '*.json', config.src + config.page + config.html]) //读取 rev-manifest.json 文件以及需要进行文件名替换的文件
+    var exec= gulp.src([config.rev + '*.json', config.src + config.page + config.html]) //读取 rev-manifest.json 文件以及需要进行文件名替换的文件
     .pipe(revCollector({
         replaceReved: false,
 		revSuffix:'',
@@ -72,7 +74,19 @@ function() {
     })) //执行文件内引用名的替换
     .pipe(utf8bom()) //添加utf8 bom编码，避免中文乱码
     .pipe(gulp.dest(config.src + config.page)); //替换后的文件输出的目录
+    if(config.update){
+	exec.pipe(gulp.dest(config.update));//将替换后的文件输出到某个指定目录，方便更新到服务器		
+    }
+    return exec;
 });
-
+gulp.task('copyResource',function(){//将改动过的静态文件，拷贝到config.update目录下，方便更新到服务器
+	var files=revCollector.getResource();
+	if(files.length>0){
+		return gulp.src(files,{base:config.src})
+		.pipe(utf8bom())
+		.pipe(gulp.dest(config.update));}
+	else
+		return null;
+});
 //任务序列
-gulp.task('default', gulpSequence('clean', 'rev', 'revCollector'));
+gulp.task('default', gulpSequence('clean', 'rev', 'revCollector','copyResource'));
