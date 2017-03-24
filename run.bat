33b30254295a0d59c3fd8886cf3c111588c75931
xuqingsen <gulangduxiao@gmail.com>
2017-03-24 16:13:36 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
 @ECHO off
 ECHO ---------------------------------------------
 call gulp default
+rem 命令行后面，传递project参数，指定运行的项目，如: call gulp default -project mkk.
 ECHO ---------------------------------------------
 ECHO Finish
 ECHO ---------------------------------------------
