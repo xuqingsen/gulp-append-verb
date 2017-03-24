291fa517cb7eae2252200c090c4f1a96541093b2
xuqingsen <gulangduxiao@gmail.com>
2017-03-24 16:10:48 +0800

    接收命令行参数 添加excludes配置
    1、接收命令行参数，调用指定项目
    2、添加excludes，排除指定的文件/文件夹

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-﻿/**
+/**
 ├── webapp/
 │   ├── css/
 │   │   ├── *.css
@@ -20,24 +20,31 @@
 │	│		├── *.ftl 
 *
 *文件路径配置
-*update:替换后的文件输出目录，方便后期统一更新到服务器,
-*src: webapp目录，绝对路径，需要自行配置
+*update: 替换后的文件输出目录，方便后期统一更新到服务器，可选
+*excludes: 排除在外的文件/文件夹，可选
+*src: 项目目录，绝对路径，需要自行配置
 *page: 页面根目录，相对路径，需要自行配置
 *resource：静态资源，相对路径，需要自行配置
 *html：页面文件，相对路径，需要自行配置
 *rev：rev-manifest.json文件所在目录，无需修改
 *app：静态资源临时目录，无需修改
 */
+
+var projects={
+    mgk: { update: 'C:\\Codes\\webform\\mgk\\update\\', src: 'C:\\Codes\\webform\\mgk\\frontpage\\', excludes: ["!C:\\Codes\\webform\\mgk\\frontpage\\xtest\\**"] },
+	jubaopen:{update:'C:\\Codes\\webform\\聚宝盆\\update\\',src:'C:\\Codes\\webform\\聚宝盆\\front\\'}
+};
+
 var config = {
-    update:'D:\\codes\\testWb\\update\\',
-    src: 'D:\\codes\\testWb\\',
     page: '',
 	resource: '**/*.{js,css,png,jpg,jpeg,gif,bmp,ico,eot,svg,ttf,woff}',
-	html: '**/*.{html,ftl,jsp,aspx}',
+	html: '**/*.{html,ftl,jsp,aspx,ascx,master}',
     rev: './rev/',
     app: './app/'
 };
-
+var argv = require('minimist')(process.argv.slice(2));
+var project = argv.project ? argv.project : "mgk";
+config=Object.assign(config,projects[project]);
 var gulp = require('gulp');
 var clean = require('gulp-clean'); //清除文件
 var rev = require('gulp-rev'); //对文件名加MD5后缀
@@ -66,27 +73,32 @@ function() {
 //替换引用
 gulp.task('revCollector',
 function() {
-    var exec= gulp.src([config.rev + '*.json', config.src + config.page + config.html]) //读取 rev-manifest.json 文件以及需要进行文件名替换的文件
-    .pipe(revCollector({
+	var revCOpts={
         replaceReved: false,
 		revSuffix:'',
 		paramPattern:"([\?|&][^'\"]*)?"
-    })) //执行文件内引用名的替换
-    .pipe(utf8bom()) //添加utf8 bom编码，避免中文乱码
+    };
+	var paths=[config.rev + '*.json', config.src + config.page + config.html];
+	if (config.excludes)
+	    paths = paths.concat(config.excludes);
+    var exec= gulp.src(paths,{base:config.src}) //读取 rev-manifest.json 文件以及需要进行文件名替换的文件
+    .pipe(revCollector(revCOpts)) //执行文件内引用名的替换
+    .pipe(utf8bom()) //添加utf8 bom编码，避免中文乱码	
     .pipe(gulp.dest(config.src + config.page)); //替换后的文件输出的目录
-    if(config.update){
-	exec.pipe(gulp.dest(config.update));//将替换后的文件输出到某个指定目录，方便更新到服务器		
-    }
-    return exec;
+	if(config.update){
+		exec.pipe(gulp.dest(config.update));//将替换后的文件输出到某个指定目录，方便更新到服务器		
+	}
+	return exec;
 });
-gulp.task('copyResource',function(){//将改动过的静态文件，拷贝到config.update目录下，方便更新到服务器
+gulp.task('copyResource',function(){
 	var files=revCollector.getResource();
 	if(files.length>0){
-		return gulp.src(files,{base:config.src})
+	return gulp.src(files,{base:config.src})
 		.pipe(utf8bom())
-		.pipe(gulp.dest(config.update));}
+	.pipe(gulp.dest(config.update));}
 	else
 		return null;
 });
+
 //任务序列
 gulp.task('default', gulpSequence('clean', 'rev', 'revCollector','copyResource'));
